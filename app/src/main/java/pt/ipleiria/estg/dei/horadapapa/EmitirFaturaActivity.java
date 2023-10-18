61dc78903136fbf7152015400762c30cdbeb48ef
Rebelo-ipl <2211914@my.ipleiria.pt>
2023-10-18 03:10:40 +0100

    Activities

---
+++ app/src/main/java/pt/ipleiria/estg/dei/horadapapa/EmitirFaturaActivity.java
@@ -0,0 +1,14 @@
+package pt.ipleiria.estg.dei.horadapapa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmitirFaturaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emitir_fatura);
+    }
+}
