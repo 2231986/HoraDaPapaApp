b9f3f7c82c34a98a0d5dbd0b8dc0dcf913205c83
Rebelo-ipl <2211914@my.ipleiria.pt>
2023-11-15 20:49:22 +0000

    SignUp Trough API
    Registo de Utilizador

--- app/src/main/java/pt/ipleiria/estg/dei/horadapapa/MainActivity.java
+++ app/src/main/java/pt/ipleiria/estg/dei/horadapapa/MainActivity.java
@@ -8,6 +8,14 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import pt.ipleiria.estg.dei.horadapapa.models.Singleton;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText etUsername, etPassword;
@@ -46,12 +54,39 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        //TODO: Verificar se o user existe, através da API
+        // Make a Volley POST request to your API endpoint for user authentication
+        String loginUrl = "http://192.168.137.235:8888/HoraDaPapa/backend/web/api/user/login"; // Replace with your actual login endpoint
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, loginUrl,
+                response -> {
+                    // Handle successful login
+                    // Maybe parse the response and determine if login was successful
+                    // If successful, navigate to the MenuActivity
+                    Intent intent = new Intent(this, MenuActivity.class);
+                    startActivity(intent);
+                },
+                error -> {
+                    // Handle error
+                    Toast.makeText(this, "Login failed!", Toast.LENGTH_SHORT).show();
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("password", password);
+                // Add other necessary parameters
+
+                return params;
+            }
+        };
+        // Add the request to the RequestQueue
+        Singleton.getInstance(this).volleyQueue.add(stringRequest);
+
+        /*//TODO: Verificar se o user existe, através da API
         if (true)
         {
             Intent activity = new Intent(this, MenuActivity.class);
             startActivity(activity);
-        }
+        }*/
     }
 
     public void onClickButtonLogin(View view) {
--- app/src/main/java/pt/ipleiria/estg/dei/horadapapa/SignUpActivity.java
+++ app/src/main/java/pt/ipleiria/estg/dei/horadapapa/SignUpActivity.java
@@ -2,13 +2,79 @@ package pt.ipleiria.estg.dei.horadapapa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import pt.ipleiria.estg.dei.horadapapa.models.Singleton;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText editTextEmail, editTextPassword, editTextName, editTextSurname, editTextNIF;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        // Initcialização te textfieds ou edittextfields
+        editTextEmail = findViewById(R.id.editTextTextEmailAddress);
+        editTextPassword = findViewById(R.id.editTextTextPassword);
+        editTextName = findViewById(R.id.editTextText);
+        editTextSurname = findViewById(R.id.editTextText2);
+        editTextNIF = findViewById(R.id.editTextText3);
+
     }
-}
+
+    public void onClickSignUp(View view) {
+
+        //ir buscar texto inserido
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+        String name = editTextName.getText().toString();
+        String surname = editTextSurname.getText().toString();
+        String nif = editTextNIF.getText().toString();
+
+        // Volley POST request to your API endpoint with this user data
+        // localhost refers to the device itself, not the server where your backend is hosted. You should use the server's IP address or domain name instead of localhost.
+        //Aqui o meu localhost é o meu ip, rede onde estou ligado, onde o apache é hosted
+        String signUpUrl = "http://192.168.137.235:8888/HoraDaPapa/backend/web/api/user/register";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, signUpUrl,
+                response -> {
+                    // Handle successful registration
+                    // Maybe navigate to the login screen
+                    Toast.makeText(this, "Signed up!", Toast.LENGTH_SHORT).show();
+                    Intent activity = new Intent(this, MainActivity.class);
+                    startActivity(activity);
+                },
+                error -> {
+                    // Handle error
+                    Log.e("SignUpError", error.toString());
+
+                }) {
+            @Override
+            // Creating a map to send as parameters in the POST request
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", name);
+                params.put("email", email);
+                params.put("password", password);
+                // Add other user data here
+
+                return params;
+            }
+        };
+
+        // Add the request to the RequestQueue
+        Singleton.getInstance(this).volleyQueue.add(stringRequest);
+    }
+    }
