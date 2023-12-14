89aad700c04c596d4460f6849cb24c9a48dc6b8d
Dinissantos <Dinissantos999@hotmail.com>
2023-12-14 22:47:50 +0000

    ongoing endpoints

--- app/src/main/java/pt/ipleiria/estg/dei/horadapapa/models/Singleton.java
+++ app/src/main/java/pt/ipleiria/estg/dei/horadapapa/models/Singleton.java
@@ -412,8 +412,16 @@ public class Singleton
             return ApiPath(context) + "plates"; //GET - Obtem todos os pratos
         }
         public static String RequestPlate(Context context, int mealID, int plateID){
-            String endpoint = ApiPath(context) + "requests/meal/{0}/plate/{1}";
-            return MessageFormat.format(endpoint, mealID + "", plateID + "");
+            String endpoint = ApiPath(context) + "meals/{0}/plate/{1}";
+            return MessageFormat.format(endpoint, mealID + "", plateID + ""); //POST - Adiciona um prato à refeição
+        }
+        public static String MealInvoice(Context context, int mealID){
+            String endpoint = ApiPath(context) + "meals/{0}/invoice";
+            return MessageFormat.format(endpoint, mealID + ""); //POST - Obtem a fatura da meal
+        }
+        public static String MealRequests(Context context, int mealID){
+            String endpoint = ApiPath(context) + "meals/{0}/requests";
+            return MessageFormat.format(endpoint, mealID + ""); //GET - Obtem os pedidos de uma refeição
         }
         public static String PlateFavorite(Context context) {
             return PlateFavorite(context, 0);
@@ -426,8 +434,59 @@ public class Singleton
             }
             else
             {
-                endpoint = ApiPath(context) + "requests/plates/{0}/favorite";
+                endpoint = ApiPath(context) + "plates/{0}/favorite";
                 endpoint = MessageFormat.format(endpoint, plateID + ""); //POST - Regista ou remove o favorito
+            }
+
+            return endpoint;
+        }
+        public static String Dinner(Context context) {
+            return PlateFavorite(context, 0);
+        }
+        public static String Dinner(Context context, int dinnerID) {
+            String endpoint = "";
+
+            if (dinnerID == 0){
+                endpoint = ApiPath(context) + "dinners/clean"; //GET - Obtem todos as mesas
+            }
+            else
+            {
+                endpoint = ApiPath(context) + "dinners/{0}/start";
+                endpoint = MessageFormat.format(endpoint, dinnerID + ""); //POST - Regista uma refeição
+            }
+
+            return endpoint;
+        }
+        public static String Helpticket(Context context) {
+            return PlateFavorite(context, 0);
+        }
+        public static String Helpticket(Context context, int helpticketID) {
+            String endpoint = "";
+
+            if (helpticketID == 0){
+                endpoint = ApiPath(context) + "helptickets"; //GET - Obtem todos as mesas
+            }
+            else
+            {
+                endpoint = ApiPath(context) + "helptickets/{0}";
+                endpoint = MessageFormat.format(endpoint, helpticketID + ""); //CRUD
+            }
+
+            return endpoint;
+        }
+        public static String Review(Context context) {
+            return PlateFavorite(context, 0);
+        }
+        public static String Review(Context context, int reviewID) {
+            String endpoint = "";
+
+            if (reviewID == 0){
+                endpoint = ApiPath(context) + "reviews"; //GET - Obtem todos as mesas
+            }
+            else
+            {
+                endpoint = ApiPath(context) + "reviews/{0}";
+                endpoint = MessageFormat.format(endpoint, reviewID + ""); //CRUD
             }
 
             return endpoint;
