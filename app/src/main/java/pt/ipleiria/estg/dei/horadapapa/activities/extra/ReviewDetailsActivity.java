90d30f9869d79c15ecb7e5b5c7158a26f4a0a783
Rebelo-ipl <2211914@my.ipleiria.pt>
2024-01-13 18:47:18 +0000

    Inicio crud reviews
    --continuar -- por pratos no spinner

---
+++ app/src/main/java/pt/ipleiria/estg/dei/horadapapa/activities/extra/ReviewDetailsActivity.java
@@ -0,0 +1,21 @@
+package pt.ipleiria.estg.dei.horadapapa.activities.extra;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import pt.ipleiria.estg.dei.horadapapa.R;
+
+public class ReviewDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_details);
+
+
+    }
+
+}
