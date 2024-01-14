6e50506d0a3465bf0ad6d23c65b7658b50aae3da
Dinissantos <Dinissantos999@hotmail.com>
2024-01-14 18:52:31 +0000

    arranjei o post

--- app/src/main/java/pt/ipleiria/estg/dei/horadapapa/models/Singleton.java
+++ app/src/main/java/pt/ipleiria/estg/dei/horadapapa/models/Singleton.java
@@ -580,11 +580,6 @@ public class Singleton {
 
     public void requestReviewAdd(Context context, int plate_id, int value, String description) {
 
-        // Log the received values
-        Log.d("ReviewDetailsActivity", "Plate ID: " + plate_id);
-        Log.d("ReviewDetailsActivity", "Rating: " + value);
-        Log.d("ReviewDetailsActivity", "Description: " + description);
-
         if (plate_id == 0) {
             BetterToast(context, "Prato inválido!");
             return;
@@ -603,26 +598,9 @@ public class Singleton {
         if (!isConnected(context)) {
             BetterToast(context, "Sem internet!");
         } else {
-            JSONObject requestBody = new JSONObject();
-
-            try {
-                if (plate_id > 0) {
-                    requestBody.put("plate_id", plate_id);
-                }
-                if (value > 0) {
-                    requestBody.put("value", value);
-                }
-                if (description != null && !description.isEmpty()) {
-                    requestBody.put("description", description);
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+            StringRequest jsonObjectRequest = new StringRequest(
                     Request.Method.POST,
                     Route.Review(context),
-                    requestBody,
                     response -> {
                         Toast.makeText(context, "Your review was submitted!!", Toast.LENGTH_SHORT).show();
                         // TODO: Implement response
@@ -631,6 +609,14 @@ public class Singleton {
                 @Override
                 public Map<String, String> getHeaders() {
                     return Route.GetAuthorizationBearerHeader(context);
+                }
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("plate_id", plate_id + "");
+                    params.put("description", description);
+                    params.put("value", value + "");
+                    return params;
                 }
             };
 
